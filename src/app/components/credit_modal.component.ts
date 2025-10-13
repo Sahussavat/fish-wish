7f24a7ab0f7e65fc21a1daab0938e3ab566d7e44
Sahussavat <mrkekeon2@gmail.com>
2025-10-13 14:09:35 +0700

    feat: add credit assets modal

---
+++ src/app/components/credit_modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'credit-modal',
+    templateUrl: './credit_modal.component.html',
+    styleUrls: ['./credit_modal.component.css'],
+})
+
+export class CreditModal {
+
+}
--- src/app/components/kratong_tab_bar.component.ts
+++ src/app/components/kratong_tab_bar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { Router } from "@angular/router";
+import { CreditModal } from "./credit_modal.component";
 
 @Component({
   selector: 'kratong-tab-bar',
   templateUrl: './kratong_tab_bar.component.html',
-  imports:[ CommonModule ],
+  imports:[ CommonModule, CreditModal ],
   styleUrls: ['./kratong_tab_bar.component.css'],
 })
 
